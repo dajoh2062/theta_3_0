31430fbea82e0c335bc83651bb1af1243d731806
dajoh2062 <dajoh2062@oslomet.no>
2025-08-25 16:29:51 +0200

    Initial commit

---
+++ src/main/java/com/example/theta_3_0/Theta30Application.java
@@ -0,0 +1,13 @@
+package com.example.theta_3_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Theta30Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Theta30Application.class, args);
+    }
+
+}
